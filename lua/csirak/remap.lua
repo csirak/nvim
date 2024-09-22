8c658472faa01e9d54f6e94d9d37be8fd13d4f70
csirak1528 <calebsirak@gmail.com>
2024-09-21 22:51:00 -0400

    made a lot of updates lol

---
+++ lua/csirak/remap.lua
@@ -0,0 +1,5 @@
+vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>nr", vim.cmd.set("relativenumber!"))
+vim.keymap.set("n", "<leader>wf", "<cmd>w<cr><esc>")
